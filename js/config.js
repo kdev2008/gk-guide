44bd39fff0290a7c2065700880edc1b01f7ec105
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 07:34:01 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.GK_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyjUdq9JJ2kfreVQXRGKc_oZJttVvXh_g0_QBwmtiCjRO3jeNRX_FliInBroGIJDdEe/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxYZtzPkHb-lHBU-qU5V_IxHmI1HavFHetAsjDNxe20yu5PbX1PFMtuvZC1MqU7LKo/exec',
   ADMIN_URL: '',
   FALLBACK_IMAGE: 'https://images.unsplash.com/photo-1521295121783-8a321d551ad2?auto=format&fit=crop&w=1400&q=80'
 };
