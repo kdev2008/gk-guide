a157857b5ce16bcbe7a76d1e9cf283589aaccacb
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 08:49:03 -0700

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.GK_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyIMnv5hLvM0EHiB5w8D3MZXhY6v4z463TLgtGh5fmbv1odAWN7Yq0IAd6u-oMF2zF5/exec',
-  ADMIN_URL: '',
-  FALLBACK_IMAGE: 'https://images.unsplash.com/photo-1521295121783-8a321d551ad2?auto=format&fit=crop&w=1400&q=80'
-};
